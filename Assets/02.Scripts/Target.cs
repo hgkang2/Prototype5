04d629eed3d9eb559b58f134c51889388c0331f7
hgkang2 <hgkang2hank@naver.com>
2025-03-24 15:48:37 +0900

    11

---
+++ Assets/02.Scripts/Target.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange =4;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
